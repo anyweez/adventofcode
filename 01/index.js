7f0fd2a9e247bd3a965ad94edad9ebfc6ebfa5a2
anyweez <luke@lukesegars.com>
2018-12-09 12:17:03 -0500

    Second star for the first problem.

--- 01/index.js
+++ 01/index.js
@@ -3,11 +3,7 @@ const fs = require('mz/fs');
 fs.readFile('01/input.txt')
     .then(resp => {
         const numbers = resp.toString().split('\n').map(str => {
-            if (str[0] === '+') {
-                return parseInt(str.slice(1), 10);
-            } else {
-                return parseInt(str);
-            }
+            return (str[0] === '+') ? parseInt(str.slice(1), 10) : parseInt(str);
         });
 
         const result = numbers.reduce((full, next) => full + next);
